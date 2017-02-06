91423c730d4187a1be7597ba50cf7fd4c5777c13
carson <375162094@qq.com>
2017-02-06 18:03:05 +0800

    优化配置项和随机数

--- src/numberKeypad.js
+++ src/numberKeypad.js
@@ -8,17 +8,17 @@
     "use strict";
     // 默认配置
     var DEFAULTS = {
-        random: false, // 是否随机摆放1-9数字
+        random: false, // 是否随机摆放 0-9 数字
         zIndex: 1000, // 弹窗的层级，可根据不同页面配置
         ciphertext: true, // 是否显示明文，默认 true 密文*  | false 明文
-        dot: false, // 是否显示小数点
+        dot: true, // 是否显示小数点
         currency: false, // 是否是货币
-        max: false, // 是否有输入的最大值，false 没有，如果有，写入实际数值，例如：999999999999.99
+        max: false, // 是否有输入的最大值，默认 false 没有，如果有，写入实际数值，必须大于0，例如：0.01 | 999999999999.99
         digits: 2, // 默认小数点保留最多2位，需要几位就写
-        type: 'password', // password | number | account
+        type: 'password', // password | number
         callback: '' // 回调
     };
-    
+
     var isIos = navigator.userAgent.match(/iPhone|iPad|iPod/i);
 
     function numberKeypad(elem, args) {
@@ -26,13 +26,13 @@
 
         this.isPassword = this.options.type === 'password';
         this.isNumber = this.options.type === 'number';
-        this.isAccount = this.options.type === 'account';
+        this.isCurrency = this.options.currency;
         this.clickEvent = 'click';
         if (isIos) {
             this.clickEvent = 'touchstart';
         }
         // 选择模板
-        this.$html = this.isNumber || this.isAccount ? $(templateNumber()) : $(templatePwd());
+        this.$html = this.isNumber ? $(templateNumber()) : $(templatePwd());
 
         this.el = elem;
         this.init();
@@ -41,7 +41,7 @@
     numberKeypad.prototype.constructor = numberKeypad;
 
     numberKeypad.prototype.init = function () {
-    		var _this = this;
+        var _this = this;
         $('body').append(this.$html);
 
         // 绑定默认关闭按钮
@@ -52,7 +52,7 @@
         this.$html.find('[data-role="ok"]').on('click', $.proxy(function () {
             var num = this.el.data('num');
             if (num) {
-                if (!this.isAccount) {
+                if (this.isCurrency) {
                     this.el.val(addZero(num, 2));
                 } else {
                     this.el.val(num);
@@ -65,19 +65,19 @@
             this.options.callback && this.options.callback(this, num);
             this.close();
         }, this));
-        
+
         // 点击遮罩层关闭
-		this.$html.on('click', function(event) {
-			if($(event.target).parents('.ui-dialog-cnt').first().length) {
-				return;
-			}
-			
-			if (_this.isPassword) {
-				_this.$html.find('[data-role="close"]').trigger('click');
-			} else {
-				_this.$html.find('.number-ok').trigger('click');
-			}
-		});
+        this.$html.on('click', function (event) {
+            if ($(event.target).parents('.ui-dialog-cnt').first().length) {
+                return;
+            }
+
+            if (_this.isPassword) {
+                _this.$html.find('[data-role="close"]').trigger('click');
+            } else {
+                _this.$html.find('.number-ok').trigger('click');
+            }
+        });
 
         // 显示弹窗
         this.$html.css('zIndex', this.options.zIndex).addClass('show');
@@ -89,16 +89,12 @@
             var mainHeight = this.$html.height();
             var cntHeight = this.$html.find('.ui-dialog-cnt').height();
 
-            if (this.isNumber || this.isAccount) {
-            		if (top >= (mainHeight - cntHeight)) {
-	                $('body').addClass('numberBody').css('marginTop', -cntHeight);
-            		}
+            if (this.isNumber) {
+                if (top >= (mainHeight - cntHeight)) {
+                    $('body').addClass('numberBody').css('marginTop', -cntHeight);
+                }
             }
         }, this), 0);
-
-        if (this.isAccount) {
-            this.$html.find('[data-key="."]').addClass('bg-gray').html('&nbsp;');
-        }
 
         if (this.isNumber) {
             this.el.val(this.el.data('num') || '');
@@ -122,14 +118,12 @@
     // 渲染数字
     numberKeypad.prototype.num = function () {
         var $box = this.$html.find('.number-box');
-        var tpl = '<div class="ui-flex ui-border-b">';
         var arr = [];
-        var len = 9;
-        // 随机 1-9
-        if (this.options.random && this.options.type != 'number' || this.isAccount) {
-            while (arr.length < 9) {
+        // 随机 0-9
+        if (this.options.random && !this.isNumber) {
+            while (arr.length < 10) {
                 //取 1-9 之间的整数
-                var num = Math.floor(9 * Math.random()) + 1;
+                var num = Math.floor(10 * Math.random());
                 var flag = false;
                 //遍历数组找到空位
                 for (var i = 0; i < arr.length; i++) {
@@ -142,29 +136,31 @@
                     arr[arr.length] = num;
                 }
             }
-            len = arr.length;
         } else {
-            // 默认 1-9
-            arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        }
-        // 渲染数字按钮
-        for (var i = 0; i < len; i++) {
-            if (i % 3 == 0 && i != 0) {
-                tpl += '</div><div class="ui-flex ui-border-b">';
-                tpl += '<a class="ui-flex-item ui-border-r" data-trigger="key" data-key="' + arr[i] + '">' + arr[i] + '</a>';
+            // 默认 1-9-0
+            if (this.isPassword) {
+            		arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
             } else {
-                tpl += '<a class="ui-flex-item ui-border-r" data-trigger="key" data-key="' + arr[i] + '">' + arr[i] + '</a>';
+                arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, (this.options.dot ? '.' : ''), 0];
             }
         }
-        tpl += '</div>';
-        $box.prepend(tpl);
+        // 渲染数字按钮
+        $box.find('[data-trigger="key"]').each(function (i) {
+            var $self = $(this);
+            var val = arr[i] + '';
+            if (val != '') {
+                $self.data('key', arr[i]).html(val);
+            } else {
+            		$self.addClass('bg-gray').html('&nbsp;');
+            }
+        });
     };
 
     // 点击数字
     numberKeypad.prototype.add = function () {
         var _this = this;
 
-        if (this.isNumber || this.isAccount) {
+        if (this.isNumber) {
             this.$html.find('[data-trigger="key"]').on(this.clickEvent, function () {
                 var $self = $(this);
 
@@ -188,7 +184,13 @@
                 }
 
                 num += key;
-
+				// 限制不能超过 max
+				if(_this.options.max && num > _this.options.max) {
+					num = num.substring(0, num.length - 1);
+					$this.el.data('num', num);
+					$this.el.val(num);
+					return false;
+				}
                 _this.el.data('num', num);
                 _this.el.val(num);
             });
@@ -225,7 +227,7 @@
     numberKeypad.prototype.del = function () {
         var _this = this;
 
-        if (this.isNumber || this.isAccount) {
+        if (this.isNumber) {
             this.$html.find('.number-delete').on(this.clickEvent, function () {
                 var num = _this.el.data('num');
                 //  数字为空的时候不在执行
@@ -315,9 +317,24 @@
 	            				<div class="pwd-val"></div>
 	            			</div>
 	            			<div class="number-box ui-border-t">
+		            			<div class="ui-flex ui-border-b">
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+		            			</div>
+		            			<div class="ui-flex ui-border-b">
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+		            			</div>
+		            			<div class="ui-flex ui-border-b">
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+		            			</div>
 	            				<div class="ui-flex ui-border-b">
 	            					<a class="ui-flex-item ui-border-r bg-gray">&nbsp;</a>
-	            					<a class="ui-flex-item ui-border-r" data-trigger="key" data-key="0">0</a>
+	            					<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
 	            					<a class="ui-flex-item ui-border-r number-delete"><i class="icon icon-delete"></i></a>
 	            				</div>
 	            			</div>
@@ -337,8 +354,23 @@
 	            		<div class="ui-dialog-bd number-bar">
 	            			<div class="number-box ui-border-t">
 	            				<div class="ui-flex ui-border-b">
-	            					<a class="ui-flex-item ui-border-r" data-trigger="key" data-key=".">.</a>
-	            					<a class="ui-flex-item ui-border-r" data-trigger="key" data-key="0">0</a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+		            			</div>
+		            			<div class="ui-flex ui-border-b">
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+		            			</div>
+		            			<div class="ui-flex ui-border-b">
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+			            			<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+		            			</div>
+	            				<div class="ui-flex ui-border-b">
+	            					<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
+	            					<a class="ui-flex-item ui-border-r" data-trigger="key"></a>
 	            					<a class="ui-flex-item ui-border-r number-hide" data-role="ok"><i class="icon icon-hide"></i></a>
 	            				</div>
 	            			</div>
